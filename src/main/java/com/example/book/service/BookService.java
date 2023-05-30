6012f26e2e85daa6e8486dd5a517500dfb6dfb2f
eunsu1 <lu0201@naver.com>
2023-05-30 16:19:07 +0900

    springboot_3

---
+++ src/main/java/com/example/book/service/BookService.java
@@ -0,0 +1,31 @@
+package com.example.book.service;
+
+import com.example.book.dto.BookDTO;
+import com.example.book.entity.BookEntity;
+import com.example.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public void save(BookDTO bookDTO) {
+//        BookEntity bookEntity = new BookEntity();
+//        bookEntity.setBookName(bookDTO.getBookName());
+//        bookEntity.setBookAuthor(bookDTO.getBookAuthor());
+//        bookEntity.setBookPrice(bookDTO.getBookPrice());
+//        BookEntity bookEntity = toSaveEntity(bookDTO);
+        BookEntity bookEntity = BookEntity.toSaveEntity(bookDTO);
+        bookRepository.save(bookEntity);
+    }
+
+//    private BookEntity toSaveEntity(BookDTO bookDTO) {
+//        BookEntity bookEntity = new BookEntity();
+//        bookEntity.setBookName(bookDTO.getBookName());
+//        bookEntity.setBookAuthor(bookDTO.getBookAuthor());
+//        bookEntity.setBookPrice(bookDTO.getBookPrice());
+//        return bookEntity;
+//    }
+}
