15d5bf1e00e63d4dacb73169279b393b7082989b
eunsu1 <lu0201@naver.com>
2023-05-31 17:59:13 +0900

    20230531

--- src/main/java/com/example/book/entity/BookEntity.java
+++ src/main/java/com/example/book/entity/BookEntity.java
@@ -3,6 +3,7 @@ package com.example.book.entity;
 import com.example.book.dto.BookDTO;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Table(name = "book_table")
+@ToString
 public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +33,15 @@ public class BookEntity {
 
     public static BookEntity toSaveEntity(BookDTO bookDTO) {
         BookEntity bookEntity = new BookEntity();
+        bookEntity.setBookName(bookDTO.getBookName());
+        bookEntity.setBookAuthor(bookDTO.getBookAuthor());
+        bookEntity.setBookPrice(bookDTO.getBookPrice());
+        return bookEntity;
+    }
+
+    public static BookEntity toUpdateEntity(BookDTO bookDTO) {
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setId(bookDTO.getId());
         bookEntity.setBookName(bookDTO.getBookName());
         bookEntity.setBookAuthor(bookDTO.getBookAuthor());
         bookEntity.setBookPrice(bookDTO.getBookPrice());
